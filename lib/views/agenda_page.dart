957eb817a64382397ba2a46b465dd06fc78beb08
usuario <gomes.amilson@gmail.com>
2022-05-26 05:20:54 -0300

    atualizado projeto

--- lib/views/agenda_page.dart
+++ lib/views/agenda_page.dart
@@ -51,7 +51,7 @@ class _AgendaPageState extends State<AgendaPage> {
                     });
                   },
                   //value: _currentUser,
-                  hint: Text('Selecione Contato'),
+                  hint: Text('Selecione Contatos'),
                 ),
               );
             }),
